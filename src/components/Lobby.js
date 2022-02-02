f1eeb265ea83c87ef8160ca024f73e9cabf4b8b4
Rahil Shah <rahil@vt.edu>
2022-02-02 18:37:40 -0500

    olalekan tijani

--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -1,25 +1,38 @@
 import React, {useState, useEffect} from 'react';
 import SwipePage from './SwipePage';
 import { socket, createWebsocket } from '../helpers/Websocket';
+import '../styles/lobby.css';
+
+const tempDate = Date.now()
 
 function Lobby() {
     // not_started, started
-    const [event, setEvent] = useState({state: "not_started"});
+    const [event, setEvent] = useState({state: "not_started", start: Date.now(), end: 6969});
+    const [now, setNow] = useState(Date.now());
+    const [count, setCount] = useState(1);
 
     // TODO: make sure connection isn't being reset on re-render or refresh (?)
-    useEffect((setEvent)=>{
-        createWebsocket();
-        setEvent({state: "started"});
+    useEffect(() => {
+        createWebsocket(setCount);
+        setEvent({ state: "started"});
     },[]);
 
+    setInterval(() => setNow(Date.now()), 10000);
+
     switch(event) {
-        case "not_started":
-            return ( <p>I JUST WALKED INTO THE LOBBY!</p> );
         case "started":
             return ( <SwipePage /> );
         default:
-            return ( <p>I JUST WALKED INTO THE LOBBY!</p> )
+            return ( 
+                <div className='lobbyContainer'>
+                    <p>I JUST WALKED INTO THE LOBBY!</p> 
+                    <p>COUNTDOWN: {now - tempDate} </p>
+                    <p>Number of People Waiting: {count} </p>
+                </div>
+            );
     }
 }
+
+
 
 export default Lobby;
--- src/helpers/Websocket.js
+++ src/helpers/Websocket.js
@@ -2,12 +2,18 @@ import { io } from "socket.io-client";
 // todo: wss for https
 var socket = null;
 
-function createWebsocket() {
-    socket = new io(process.env.WS_SERVER_URL || 'ws://localhost:4000/ws/', { auth: {token: localStorage.getItem("token")} });
+function createWebsocket(setCount) {
+    socket = new io(process.env.WS_SERVER_URL || 'ws://localhost:4000/', { auth: {token: localStorage.getItem("token"), } });
+    // socket.onAny((event, ...args) => {
+    //     console.log(event, args);
+    //     console.log("shit")
+    // });
 
-    socket.on("connect", () => {
+    socket.on("connection", (n) => {
         // we want to ask the server for the state of the "Event"
-        console.log(socket.connected); // true
+        console.log("whoaaaaa");
+        console.log("num connected: ", n); // true
+        setCount(n);
     });
     
     socket.on("event_state", (state) => {
